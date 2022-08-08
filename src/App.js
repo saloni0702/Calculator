737cbc92be58fd3619f259e5dc9c3d711f196eaf
Saloni <patelsaloni88@gmail.com>
2022-08-08 21:46:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     {
       setResult(result.slice(0, result.length-1));   
     }
-    else if(e.key==="." || e.key==="0" || e.key==="1" || e.key==="2" || e.key==="3" || e.key==="4" ||e.key==="5" || e.key==="6" || e.key==="7" || e.key==="8" || e.key==="9" || e.key==="+" || e.key==="-" || e.key==="*" || e.key==="/" ||)
+    else if(e.key==="." || e.key==="0" || e.key==="1" || e.key==="2" || e.key==="3" || e.key==="4" ||e.key==="5" || e.key==="6" || e.key==="7" || e.key==="8" || e.key==="9" || e.key==="+" || e.key==="-" || e.key==="*" || e.key==="/")
     {
       setResult(result.concat(e.key));
     }
